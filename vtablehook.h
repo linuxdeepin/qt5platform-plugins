c2f9bd059bd4b4464f3049f1bc9aed0205b19736
zccrs <ccrr1314@live.com>
2016-09-02 12:27:15 +0800

    Fix crash on 32bit system
    Change-Id: I1ad620a5910466cc4a4ca6c11fb93b6d177090ef

--- vtablehook.h
+++ vtablehook.h
@@ -8,7 +8,7 @@
 class VtableHook
 {
 public:
-    static bool copyVtable(qintptr **obj);
+    static bool copyVtable(quintptr **obj);
     static bool clearGhostVtable(void *obj);
 
     template <typename List1, typename List2> struct CheckCompatibleArguments { enum { value = false }; };
@@ -17,14 +17,14 @@ public:
     static bool overrideVfptrFun(const typename QtPrivate::FunctionPointer<Fun1>::Object *t1, Fun1 fun1,
                       const typename QtPrivate::FunctionPointer<Fun2>::Object *t2, Fun2 fun2)
     {
-        if (!objToOriginalVfptr.contains((qintptr**)t1) && !copyVtable((qintptr**)t1))
+        if (!objToOriginalVfptr.contains((quintptr**)t1) && !copyVtable((quintptr**)t1))
             return false;
 
         //! ({code}) in the form of a code is to eliminate - Wstrict - aliasing build warnings
-        qintptr fun1_offset = ({qintptr *f = (qintptr *)&fun1; *f - 1;});
-        qintptr fun2_offset = ({qintptr *f = (qintptr *)&fun2; *f - 1;});
+        quintptr fun1_offset = ({quintptr *f = (quintptr *)&fun1; *f - 1;});
+        quintptr fun2_offset = ({quintptr *f = (quintptr *)&fun2; *f - 1;});
 
-        if (fun1_offset < 0 || fun1_offset > UINT_LEAST16_MAX || fun2_offset < 0)
+        if (fun1_offset < 0 || fun1_offset > UINT_LEAST16_MAX)
             return false;
 
         typedef QtPrivate::FunctionPointer<Fun1> FunInfo1;
@@ -36,13 +36,13 @@ public:
         Q_STATIC_ASSERT_X((CheckCompatibleArguments<QtPrivate::List<typename FunInfo1::ReturnType>, QtPrivate::List<typename FunInfo2::ReturnType>>::value),
                           "Function1 and Function2 return type are not compatible..");
 
-        qintptr *vfptr_t1 = *(qintptr**)t1;
-        qintptr *vfptr_t2 = *(qintptr**)t2;
+        quintptr *vfptr_t1 = *(quintptr**)t1;
+        quintptr *vfptr_t2 = *(quintptr**)t2;
 
         if (fun2_offset > UINT_LEAST16_MAX)
-            *(qintptr*)((char*)vfptr_t1 + fun1_offset) = fun2_offset + 1;
+            *(quintptr*)((char*)vfptr_t1 + fun1_offset) = fun2_offset + 1;
         else
-            *(qintptr*)((char*)vfptr_t1 + fun1_offset) = *(qintptr*)((char*)vfptr_t2 + fun2_offset);
+            *(quintptr*)((char*)vfptr_t1 + fun1_offset) = *(quintptr*)((char*)vfptr_t2 + fun2_offset);
 
         return true;
     }
@@ -50,27 +50,27 @@ public:
     template<typename Fun1>
     static bool resetVfptrFun(const typename QtPrivate::FunctionPointer<Fun1>::Object *t1, Fun1 fun1)
     {
-        qintptr *vfptr_t2 = objToOriginalVfptr.value((qintptr**)t1, 0);
+        quintptr *vfptr_t2 = objToOriginalVfptr.value((quintptr**)t1, 0);
 
         if (!vfptr_t2)
             return false;
 
         //! ({code}) in the form of a code is to eliminate - Wstrict - aliasing build warnings
-        qintptr fun1_offset = ({qintptr *f = (qintptr *)&fun1; *f - 1;});
+        quintptr fun1_offset = ({quintptr *f = (quintptr *)&fun1; *f - 1;});
 
         if (fun1_offset < 0 || fun1_offset > UINT_LEAST16_MAX)
             return false;
 
-        qintptr *vfptr_t1 = *(qintptr**)t1;
+        quintptr *vfptr_t1 = *(quintptr**)t1;
 
-        *(qintptr*)((char*)vfptr_t1 + fun1_offset) = *(qintptr*)((char*)vfptr_t2 + fun1_offset);
+        *(quintptr*)((char*)vfptr_t1 + fun1_offset) = *(quintptr*)((char*)vfptr_t2 + fun1_offset);
 
         return true;
     }
 
 private:
-    static QHash<qintptr**, qintptr*> objToOriginalVfptr;
-    static QHash<void*, qintptr*> objToGhostVfptr;
+    static QHash<quintptr**, quintptr*> objToOriginalVfptr;
+    static QHash<void*, quintptr*> objToGhostVfptr;
 };
 
 #endif // VTABLEHOOK_H
