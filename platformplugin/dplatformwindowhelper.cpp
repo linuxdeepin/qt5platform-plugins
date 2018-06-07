b779925639c51b9024023797c536bc7bd61805ec
zccrs <ccrr1314@live.com>
2018-06-07 09:27:33 +0800

    fix: can not get focus by mouse press on BypassWindowManagerHint window
    Change-Id: Id7e250106df1dc8d514558679189d6ffff89bbe0

--- platformplugin/dplatformwindowhelper.cpp
+++ platformplugin/dplatformwindowhelper.cpp
@@ -669,6 +669,22 @@ bool DPlatformWindowHelper::eventFilter(QObject *watched, QEvent *event)
             }
             break;
         }
+#if QT_VERSION > QT_VERSION_CHECK(5, 6, 1)
+        // NOTE(zccrs): https://codereview.qt-project.org/#/c/162027/ (QTBUG-53993)
+        case QEvent::MouseButtonPress: {
+            const QMouseEvent *me = static_cast<QMouseEvent*>(event);
+            QWindow *w = m_nativeWindow->window();
+
+            if (me->button() == Qt::LeftButton && w != QGuiApplication::focusWindow()) {
+                if (!(w->flags() & (Qt::WindowDoesNotAcceptFocus))
+                        && w->type() != Qt::ToolTip
+                        && w->type() != Qt::Popup) {
+                    w->requestActivate();
+                }
+            }
+            break;
+        }
+#endif
         case QEvent::MouseButtonRelease: {
             if (m_frameWindow->m_isSystemMoveResizeState) {
                 Utility::cancelWindowMoveResize(Utility::getNativeTopLevelWindow(m_frameWindow->winId()));
