gdmec07150651/qt5dxcb-plugin

fix: timer interval according to upstream Qt
Ref: https://codereview.qt-project.org/c/qt/qtbase/+/267242

platformplugin/windoweventhook.cpp
@@ -438,7 +438,7 @@ void WindowEventHook::handleFocusOutEvent(const xcb_focus_out_event_t *event)
     // There is however no equivalent for XPutBackEvent so register a
     // callback for QXcbConnection instead.
 #if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
-    xcbWindow->connection()->focusInTimer().start(400);
+    xcbWindow->connection()->focusInTimer().start(100);
 #else
     xcbWindow->connection()->addPeekFunc(focusInPeeker);
 #endif

linuxdeepin/qt5platform-plugins

xcb/windoweventhook.cpp
@@ -437,7 +437,7 @@ void WindowEventHook::handleFocusOutEvent(QXcbWindowEventListener *el, const xcb
     // There is however no equivalent for XPutBackEvent so register a
     // callback for QXcbConnection instead.
 #if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
-    window->connection()->focusInTimer().start(400);
+    window->connection()->focusInTimer().start(100);
 #else
     window->connection()->addPeekFunc(focusInPeeker);
 #endif