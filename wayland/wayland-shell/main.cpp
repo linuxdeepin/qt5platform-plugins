116d510543897109ce6e4457e8de8109f10f226f
993381 <56956885+993381@users.noreply.github.com>
2022-05-19 09:00:47 +0000

    chore: 支持v23版本创建xdg-shell (#96)
    v23版本xdg-shell-v6废弃，创建失败则创建xdg-shell
    
    Log:
    Influence:
    
    Co-authored-by: AlexOne <wangpenga@uniontech.com>

--- wayland/wayland-shell/main.cpp
+++ wayland/wayland-shell/main.cpp
@@ -44,6 +44,12 @@ QWaylandShellIntegration *QKWaylandShellIntegrationPlugin::create(const QString 
     Q_UNUSED(paramList)
     auto wayland_integration = static_cast<QWaylandIntegration *>(QGuiApplicationPrivate::platformIntegration());
     auto shell = wayland_integration->createShellIntegration("xdg-shell-v6");
+    if (!shell) {
+        shell = wayland_integration->createShellIntegration("xdg-shell");
+    }
+    if (!shell) {
+        return nullptr;
+    }
 
     HookOverride(shell, &QWaylandShellIntegration::createShellSurface, DWaylandShellManager::createShellSurface);
 
