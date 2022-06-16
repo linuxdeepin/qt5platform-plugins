b954d47b61ef48f12826d3fef8182d0c0d346aa6
AlexOne <wangpenga@uniontech.com>
2022-06-16 16:55:34 +0800

    chore: Qt5.11 版本兼容性测试提示信息
    之前加了兼容性测试程序，程序未兼容低版本，报错信息不明显
    
    Log:
    Influence: none

--- wayland/wayland-shell/config.tests/wayland_test/main.cpp
+++ wayland/wayland-shell/config.tests/wayland_test/main.cpp
@@ -1,20 +1,25 @@
-#include <KWayland/Client/registry.h>
 #include <private/qguiapplication_p.h>
 #include <private/qwaylanddisplay_p.h>
 #include <private/qwaylandintegration_p.h>
 #include <private/qwaylandwindow_p.h>
-#include <private/qxkbcommon_p.h>
 #include <qpa/qplatformcursor.h>
 #include <qpa/qplatforminputcontext.h>
 #include <qpa/qplatformintegration.h>
 #include <qpa/qwindowsysteminterface.h>
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
+#include <KWayland/Client/registry.h>
+#include <private/qxkbcommon_p.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
     QWaylandDisplay *display =
         static_cast<QWaylandIntegration *>(
             QGuiApplicationPrivate::platformIntegration())
             ->display();
     display->usingInputContextFromCompositor();
+#endif
     return 0;
 }
