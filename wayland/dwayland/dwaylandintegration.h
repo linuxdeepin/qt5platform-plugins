7165c7126ac98638558b94d605f812e50e9dcdd6
范朋程 <fanpengcheng@uniontech.com>
2022-08-02 13:43:35 +0800

    fix: 当主屏发生变化后，及时更新到xsettings
    dwayland通过xsettings获取主屏信息的变化，用于实现wayland下主屏设置功能
    确保qt接口和display服务的主屏数据一致
    
    Log: 解决wayland下主屏功能异常问题
    Influence: 当主屏发生变化后，及时更新到xsettings
    Bug: https://pms.uniontech.com/bug-view-146451.html
    Change-Id: I25ad8568e16e2b40534a036688866af25910342d

--- wayland/dwayland/dwaylandintegration.h
+++ wayland/dwayland/dwaylandintegration.h
@@ -39,6 +39,10 @@ public:
     void initialize() override;
     QStringList themeNames() const override;
     QVariant styleHint(StyleHint hint) const override;
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 12, 0)
+    void setPrimaryScreen(QPlatformScreen *newPrimary);
+#endif
 };
 
 DPP_END_NAMESPACE
