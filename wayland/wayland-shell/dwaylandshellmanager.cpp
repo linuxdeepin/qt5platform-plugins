309040cee64facc106383e2ff599c2b22d9e77e6
zhangkun <zhangkun2@uniontech.com>
2023-09-11 13:40:04 +0800

    fix: 修复Wayland下Popup类型的窗口，使用_d_dwayland_window-type自定义窗口role不生效
    自定义后，当再次设置属性，会被重新设置会ToolTip，导致设置不生效，使用属性控制设置只能生效一次
    
    Log: 修复Wayland下Popup类型的窗口，使用_d_dwayland_window-type自定义窗口role不生效
    Bug: https://pms.uniontech.com/bug-view-193877.html
    Influence: wayland下自定义窗口role
    Change-Id: If60cd0245cd6b62b38322cf3afb5f2990b182501

--- wayland/wayland-shell/dwaylandshellmanager.cpp
+++ wayland/wayland-shell/dwaylandshellmanager.cpp
@@ -229,8 +229,10 @@ void DWaylandShellManager::sendProperty(QWaylandShellSurface *self, const QStrin
     }
 
     // 将popup的窗口设置为tooltop层级, 包括qmenu，combobox弹出窗口
-    if (wlWindow->window()->type() == Qt::Popup)
+    if (wlWindow->window()->type() == Qt::Popup && !ksurface->property("defaultRoleHasBeenSet").isValid()) {
         ksurface->setRole(PlasmaShellSurface::Role::ToolTip);
+        ksurface->setProperty("defaultRoleHasBeenSet", true);
+    }
 
 #ifdef D_DEEPIN_KWIN
     // 禁止窗口移动接口适配。
