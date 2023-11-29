c84d6f7e17d1710c655a9676091a8ee2a0c76b1f
ck <chenke@deepin.org>
2023-11-29 16:53:14 +0800

    fix(build): qt6 build failed
    switch quantity not an integer

--- src/dnativesettings.cpp
+++ src/dnativesettings.cpp
@@ -147,7 +147,7 @@ void DNativeSettings::init(const QMetaObject *metaObject)
         QMetaPropertyBuilder op;
 
 #if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
-        switch (mp.metaType()) {
+        switch (mp.typeId()) {
 #else
         switch (static_cast<int>(mp.type())) {
 #endif
